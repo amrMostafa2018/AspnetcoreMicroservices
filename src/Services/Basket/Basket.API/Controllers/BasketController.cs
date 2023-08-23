2057c5cf6ba4ce3eb0695fbe46b38440c54a2b3e
Ayman Fouad <ayman.fouad@excelsystems-eg.com>
2023-08-23 15:42:54 +0300

    Basket Microservice

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,43 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(IEnumerable<ShoppingCart>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<ShoppingCart>>> GetBasket(string userName)
+        {
+            var basket = await _basketRepository.GetBasket(userName);
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(IEnumerable<ShoppingCart>), StatusCodes.Status200OK)]
+        public async Task<ActionResult> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _basketRepository.UpdateBasket(basket));
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _basketRepository.DeleteBasket(userName);
+            return Ok();
+        }
+    }
+}
