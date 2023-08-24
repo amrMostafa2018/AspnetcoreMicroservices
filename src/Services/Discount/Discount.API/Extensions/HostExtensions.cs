faf5ba99bddaaac85fa4003546b2e16d6a324bb3
Amr.Mostafa <Amr.Mostafa@AMRMOSTAFA>
2023-08-24 15:11:11 +0300

    migrate data in DiscounDb

---
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+
+using System;
+
+namespace Discount.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrteDateBase<TContext>(this IHost host, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var service = scope.ServiceProvider;
+                var configuration = service.GetRequiredService<IConfiguration>();
+                var logger = service.GetRequiredService<ILogger<TContext>>();
+
+                try
+                {
+                    using var connection = new NpgsqlConnection
+                        (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                    connection.Open();
+
+                    using var command = new NpgsqlCommand
+                    {
+                        Connection = connection,
+                    };
+                    command.CommandText = "Drop table If EXISTS Coupon";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"Create Table Coupon (Id SERIAL PRIMARY KEY,
+                                                                 ProductName VARCHAR(24) NOT NULL,
+                                                                 DESCRIPTION TEXT,
+                                                                 Amount INT)";
+                    command.ExecuteNonQuery();
+
+                }
+                catch(Exception ex)
+                {
+                    throw;
+                }
+                return host;
+            }
+        }
+    }
+}
