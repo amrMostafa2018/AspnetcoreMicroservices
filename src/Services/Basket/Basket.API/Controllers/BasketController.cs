3e26cb3207724dd4b25cc8072a86313d9a352533
Amr.Mostafa <Amr.Mostafa@AMRMOSTAFA>
2023-08-27 10:12:20 +0300

    add DiscountGrpcService and Registry  DiscountProtoService.DiscountProtoServiceClient in Startup

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,9 +13,12 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _basketRepository;
-        public BasketController(IBasketRepository basketRepository)
+        private readonly DiscountGrpcService _discountGrpcService;
+        public BasketController(IBasketRepository basketRepository,
+                                DiscountGrpcService discountGrpcService)
         {
             _basketRepository = basketRepository;
+            _discountGrpcService = discountGrpcService;
         }
 
         [HttpGet("{userName}", Name = "GetBasket")]
@@ -31,8 +35,12 @@ namespace Basket.API.Controllers
         {
             //TODO : Communicate with Discount.Grpc
             //and after that calculate latest prices of products into shopping cart
-
-
+            //consume Discount Grpc
+            foreach (var item in basket.Items)
+            {
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount;
+            }
 
             return Ok(await _basketRepository.UpdateBasket(basket));
         }
