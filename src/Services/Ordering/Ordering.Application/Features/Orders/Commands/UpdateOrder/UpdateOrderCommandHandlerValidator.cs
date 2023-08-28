30a1334499945d916ecfd95dfaad88b661c38709
Amr.Mostafa <Amr.Mostafa@AMRMOSTAFA>
2023-08-28 10:16:54 +0300

    add Delete Order and Update Order Commands

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandlerValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandlerValidator : AbstractValidator<UpdateOrderCommand>
+    {
+        public UpdateOrderCommandHandlerValidator()
+        {
+            RuleFor(n => n.UserName)
+                .NotEmpty()
+                .WithMessage("{UserName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{UserName} must not exceed 50 characters.");
+
+            RuleFor(n => n.EmailAddress)
+                .NotEmpty()
+                .WithMessage("{EmailAddress} is required.");
+
+            RuleFor(n => n.TotalPrice)
+              .NotEmpty()
+              .WithMessage("{TotalPrice} is required.")
+              .GreaterThan(0).WithMessage("{TotalPrice} should be greater than Zero.");
+        }
+    }
+}
