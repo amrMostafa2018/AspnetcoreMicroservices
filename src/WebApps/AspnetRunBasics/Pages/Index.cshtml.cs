219f16ab3c7318f5321d0678f096ea44cc545890
Ayman Fouad <ayman.fouad@excelsystems-eg.com>
2023-08-31 13:44:56 +0300

    add AspnetRunBasic

---
+++ src/WebApps/AspnetRunBasics/Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspnetRunBasics.Repositories;
+using AspnetRunBasics.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspnetRunBasics.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IBasketService _basketService;
+        private readonly IProductRepository _productRepository;
+        private readonly ICartRepository _cartRepository;
+
+        public IndexModel(IProductRepository productRepository, 
+                          ICartRepository cartRepository,
+                          IBasketService basketService)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+            _cartRepository = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
+            _basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
+        }
+
+        public IEnumerable<Entities.Product> ProductList { get; set; } = new List<Entities.Product>();
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var baskets = await _basketService.GetBasket("swn");
+            ProductList = await _productRepository.GetProducts();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAddToCartAsync(int productId)
+        {
+            //if (!User.Identity.IsAuthenticated)
+            //    return RedirectToPage("./Account/Login", new { area = "Identity" });
+
+            await _cartRepository.AddItem("test", productId);
+            return RedirectToPage("Cart");
+        }
+    }
+}
